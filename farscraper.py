0435920347e5aa35e780ecbc14cf11aefb94ab9b
Shawn <vahnatai@users.noreply.github.com>
2026-03-30 12:31:33 -0230

    minor: PEP 8 colon spacing

--- farscraper.py
+++ farscraper.py
@@ -21,55 +21,55 @@ argp = argparse.ArgumentParser(
 argp.add_argument('page_title', type=str, nargs="?")
 argp.add_argument('count', type=int, nargs="?", default=DEFAULT_COUNT)
 
-def describe(page) :
+def describe(page):
     match = _EPISODE_RE.search(page.html)
     
-    if match :
+    if match:
         season_number = match.group(1)
         episode_number = match.group(2)
         print('FAR {0}x{1} "{2}"'.format(season_number, episode_number.zfill(2), page.title))
-    else :
+    else:
         print('MOV "{0}"'.format(page.title))
     sys.stdout.flush()
 
-def get_next_released(page) :
+def get_next_released(page):
     match = _NEXT_RE.search(page.html)
     
-    if match :
+    if match:
         next_title = match.group(1)
         next = fandom.page(next_title)
         return next
-    else :
+    else:
         raise ValueError('Could not find next episode link')
 
-if __name__ == '__main__' :
+if __name__ == '__main__':
     args = argp.parse_args()
 
     count = args.count
 
-    if args.page_title :
+    if args.page_title:
         page_title = args.page_title
-    else :
+    else:
         page_title = FIRST_EP_NAME
         page = fandom.page(page_title)
         describe(page)
         count -= 1
 
-    try :
+    try:
         page = fandom.page(page_title)
-    except fandom.error.PageError as e :
+    except fandom.error.PageError as e:
         print(e)
         exit()
 
-    for i in range(count) :
-        try :
+    for i in range(count):
+        try:
             page = get_next_released(page)
-            if page.title == FIRST_COMIC_NAME :
+            if page.title == FIRST_COMIC_NAME:
                 # TODO comics listing
                 print('comics listing not yet implemented...')
                 exit()
             describe(page)
-        except Exception as e :
+        except Exception as e:
             print('Exception:', e)
             traceback.print_exception(e)
             exit()
